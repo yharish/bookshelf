63d6364504ede31eb626ef9ea11ef361526b220f
Yashaswini Harish <yashaswinik@gmail.com>
2013-09-07 22:39:05 -0700

    Added static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Bookshelf::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/help"
+
+  get "static_pages/about"
+
+  get "static_pages/terms"
+
+  get "static_pages/privacy"
+
   resources :users
   resources :books
 
